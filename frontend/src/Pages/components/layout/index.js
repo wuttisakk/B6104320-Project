90d845127ccad91048cc871379c7228b6a05420c
wuttisakk <kuchitzkuchita@gmail.com>
2021-09-09 21:12:00 +0700

    push backend and frontend

---
+++ frontend/src/Pages/components/layout/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../header';
+
+const Layout = (props) => {
+  return(
+    <div>
+        <Header />
+        {props.children}
+    </div>
+   )
+
+ }
+
+export default Layout
